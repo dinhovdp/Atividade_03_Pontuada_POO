78b6e03b7c7f19c87c747f8f8ab37d6af275788b
Edson Silva <dinhovdp@gmail.com>
2026-06-26 23:07:57 -0300

    :tada: inicializando

--- src/exercicios_01/Administrador.java
+++ src/exercicios_01/Administrador.java
@@ -1,10 +1,39 @@
 package exercicios_01;
 
-public class Administrador {
+public abstract class Administrador extends Usuario{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		
+		public Administrador(String nome, String email) {
+			super(nome, email);
+		
 	}
+		private	String nivelDeAcesso;
+			
+	 public Administrador(String nome, String email, String nivelDeAcesso) {
+			super(nome, email);
+				this.nivelDeAcesso = nivelDeAcesso;
+		}
+
+	 public String getNivelDeAcesso() {
+		 return nivelDeAcesso;
+	 }
+
+	 public void setNivelDeAcesso(String nivelDeAcesso) {
+		 this.nivelDeAcesso = nivelDeAcesso;
+	 }
+
+	
+	 public void visualizar() {
+	
+			System.out.println("*****************************************************");
+			System.out.println("**		DADOS DE USUARIOS			   **");
+			System.out.println("*****************************************************");
+			
+			System.out.printf("**		Nivel de Acesso: %s %s", this.nivelDeAcesso);
+			
+			System.out.println("*****************************************************");
+
+	 }
 
 }
--- src/exercicios_01/Usuario.java
+++ src/exercicios_01/Usuario.java
@@ -1,10 +1,77 @@
 package exercicios_01;
 
-public class Usuario {
+public  class Usuario {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+	/* ATRIBUTOS DA CLASSE
+	 * modificadores de Acesso será " private " deve ser colocado antes do tipo da variavel
+	 */
+	
+		private String nome = "";
+		private String email = "";
+		
+				
+	/*Metodo construtor - Gerar as intancias (objetos) da Classe
+	 *  Generete constructor fields
+	 */
+		
+		public Usuario(String nome, String email) {
+			this.nome = nome;
+			this.email = email;
+			
+		}
+
+		/* SouRce - Generate getts and setters - select all 
+		 * 
+		 */
+
+		/* metodo Get, consegue visualizar 
+		 * metodo Set Modificar os dados
+		 */
+
+		public String getNome() {
+			return nome;
+			
+		}
+
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+
+		public String getEmail() {
+			return email;
+			
+		}
+
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		
+		
+		//23-06 polimorfismo - sobreescrita de um novo meotodo
+		
+		public void visualizar() {
+			
+			//para facilitar a visualização para o usuario vamos usar...
+			
+		
+	
+			/* METODO RELACIONADO AO OBJETO
+			 * 
+			 */
+		
+		
+				
+				System.out.println("*****************************************************");
+				System.out.println("**		DADOS DE USUARIOS			   **");
+				System.out.println("*****************************************************");
+				System.out.printf("**		Nome do Usuario: %s %s", this.nome);
+				System.out.printf("**		Dados  do e-mail: %s %s", this.email);
+				System.out.println("*****************************************************");
+		
+		}
 
 }
