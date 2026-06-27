45646a03f77d8ff83f1e5d35490b2534b6c89b9b
Edson Silva <dinhovdp@gmail.com>
2026-06-27 02:03:43 -0300

    :tada: inicializando

--- src/exercicios_01/TestaUsuarios.java
+++ src/exercicios_01/TestaUsuarios.java
@@ -4,14 +4,54 @@ public class TestaUsuarios {
 
     public static void main(String[] args) {
 
-    	Usuario usuario = new Usuario("Rosenildo", "Ro.zenildo@email@.com");
-        usuario.visualizar();
     	
-        Usuario usuario1 = new Usuario("Edson", "edshow.10@email.com");
-        usuario1.visualizar();
-
-        Administrador admin = new Administrador("Stefanne", "stefy.20@email.com", "ADMIN");
-        admin.visualizar();
-
+    	try {
+    		Usuario usuario = new Usuario("Rosenildo", "Ro.zenildo@email@.com");
+            usuario.visualizar();
+    	} catch (IllegalArgumentException e) {
+    	    System.out.println("Erro: " + e.getMessage());
+    	    
+    	 try {
+    		 Usuario usuario1 = new Usuario("Lilian", "@email.com");
+    		 	usuario1.visualizar();
+    	 			} catch (IllegalArgumentException e) {
+    	 				System.out.println("Erro: " + e.getMessage());
+    	    
+    	 try { 
+    		 Usuario usuario2 = new Usuario("Edson", "edshow.10@email.com");
+    		 	usuario2.visualizar();
+    	 			} catch (IllegalArgumentException e) {
+    	 				System.out.println("Erro: " + e.getMessage());
+    	    
+    	 try {  
+    		 Usuario usuario3 = new Usuario("Yoga", " execução.aurora@titanic.com");
+    		 	usuario3.visualizar();
+    	 			} catch (IllegalArgumentException e) {
+    	 				System.out.println("Erro: " + e.getMessage());
+            
+    	 try {  
+    		 Administrador admin = new Administrador ("Naruto", " proximo.hokage@viladafolha.org", "ADMIN");
+    		 	admin.visualizar();
+    	 			} catch (IllegalArgumentException e) {
+    	 				System.out.println("Erro: " + e.getMessage());
+            
+    	 try {  
+    		 Administrador admin2 = new Administrador("Seiya", "meteoro.de.pegasus@deusa.Atena.ong", "ADMIN");
+            	admin2.visualizar();
+    	 			} catch (IllegalArgumentException e) {
+    	 				System.out.println("Erro: " + e.getMessage());
+    	    
+    	    
+    	 try {  
+    		 Administrador admin3 = new Administrador(".", "anonimo@stealth.off", "ADMIN");
+    		 	admin3.visualizar();        
+    	 			} catch (IllegalArgumentException e) {
+    	 				System.out.println("Erro: " + e.getMessage());
+    	}
     }
 }
+    	
+    	
+      
+
+    
--- src/exercicios_01/Validacao.java
+++ src/exercicios_01/Validacao.java
@@ -2,20 +2,16 @@ package exercicios_01;
 
 public class Validacao {
 
-	
-		public static void validarNome(String nome) {
-
+		
+		  
+			public static void validarNome(String nome) {
+				if (nome == null || nome.trim().isEmpty())
+					throw new IllegalArgumentException("Nome não pode ser vazio!");
+			}
 			
-			
-		    if (nome == null || nome.trim().isEmpty()) {
-		        throw new IllegalArgumentException("Nome inválido");
-		    }
-
-		    
-		    
-		    
-		    
-		    
+			public static void validarEmail(String email) {
+				if (email == null || !email.matches("^[a-zA-Z0-9._%+\\-]+@[a-zA-Z0-9\\-]+(?:\\.[a-zA-Z0-9\\-]+)*\\.[a-zA-Z]{2,}$"))
+					throw new IllegalArgumentException("Digite um e-mail válido!");
 		}
 		
 }
